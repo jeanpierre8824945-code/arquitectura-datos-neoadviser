815f8318eb26c4927a07735a3e6e0d36725f8cbb
Jean Pierre Martinez Manrique <jeanpierre8824945@gmail.com>
2026-09-05 18:05:23 -0500

    se agrego, video, diagrama, texto con diseño.

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span><strong>Arquitectura E-commerce IA</strong></span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
